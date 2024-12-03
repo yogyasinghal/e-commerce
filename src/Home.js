135e6e573e797e3849366021999d171811175b4a
yogyasinghal <yogyasinghal21@gmail.com>
2024-12-03 13:58:12 +0530

    latest

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,33 @@
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
 
-function Home() {
-    return (
-      <div className='App'>
+// function Home() {
+//     return (
+//       <div className='App'>
      
-        <h1>End your Products Search Here</h1>
-        <div className = "justify-content-evenly m-4 p-2" style = {{display:"flex"}}>
+//         <h1>End your Products Search Here</h1>
+//         <div className = "justify-content-evenly m-4 p-2" style = {{display:"flex"}}>
         
-        <Link className='btn btn-primary ' to='/login'>Login</Link>
+//         <Link className='btn btn-primary ' to='/login'>Login</Link>
        
         
-        </div>
+//         </div>
     
         
-      </div>
-    );
-  }
+//       </div>
+//     );
+//   }
   
-  export default Home;
+//   export default Home;
+
+import React from 'react';
+
+const Home =() =>{
+
+
+  return(
+    <div className="home">
+        <h1>Welcome to Connectwise Dashboard.</h1>
+    </div>
+    );
+}
+export default Home;
